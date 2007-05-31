a4331df8827963fd52b59eb8d07a425b3a984940
Nick <devnull@localhost>
2007-05-31 01:55:16 +0000

    Corrected the proper w3c doctypes

--- projects/xedlib3/lib/h_template.php
+++ projects/xedlib3/lib/h_template.php
@@ -161,10 +161,10 @@ class Template
 			if (isset($attribs['TYPE']))
 			{
 				if ($attribs['TYPE'] == 'strict')
-					$output = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN"
-						"http://www.w3.org/TR/html4/strict.dtd">';
+					$output = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+						"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
 				if ($attribs['TYPE'] == 'trans')
-					$output = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+					$output = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 						"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
 			}
 		}
