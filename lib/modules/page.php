410039228361c527fd6a23d889a40c7c365727a2
Xedecimal <devnull@localhost>
2010-06-18 04:42:34 +0000

    Page can now retrieve only a part using /part/pagename.

--- lib/modules/page.php
+++ lib/modules/page.php
@@ -7,9 +7,12 @@ class ModPage extends Module
 		global $_d;
 
 		$name = @$_d['q'][0];
+
+		if ($name == 'part') $name = $_d['q'][1];
 		$file = "content/{$name}.xml";
 
 		$content = @file_get_contents($file);
+		if ($_d['q'][0] == 'part') die($content);
 		return '<div class="page_content">'.$content.'</div>';
 	}
 }
