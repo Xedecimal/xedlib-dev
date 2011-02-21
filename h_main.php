60a7d36d7cb979162985464bbb3583338983c8bf
Xedecimal <Xedecimal@gmail.com>
2011-02-21 11:23:19 -0800

    This is all set elsewhere now.

--- h_main.php
+++ h_main.php
@@ -5,13 +5,7 @@ require_once('lib/classes/File.php');
 require_once('lib/classes/data/Database.php');
 require_once('lib/classes/data/DataSet.php');
 
-$_d['me'] = Server::GetVar('SCRIPT_NAME');
-$_d['app_dir'] = __DIR__;
-$_d['app_abs'] = Server::GetRelativePath($_d['app_dir']);
-$_d['page_head'] = '';
-$_d['page_title'] = 'Xedlib Tests';
-
-$_d['settings']['database'] = 'mysqli://root:ransal@localhost/test';
+$_d['settings']['database'] = 'mysqli://root:ransal@127.0.0.1/test';
 
 $db = new Database();
 $db->Open($_d['settings']['database']);
