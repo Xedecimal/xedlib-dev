9557bb9a8898de66eb49c7e68e4542da5f3fdf15
Xedecimal <Xedecimal@gmail.com>
2011-02-21 11:22:49 -0800

    Added dataset to the main test file.

--- h_main.php
+++ h_main.php
@@ -3,6 +3,7 @@
 require_once('lib/classes/Server.php');
 require_once('lib/classes/File.php');
 require_once('lib/classes/data/Database.php');
+require_once('lib/classes/data/DataSet.php');
 
 $_d['me'] = Server::GetVar('SCRIPT_NAME');
 $_d['app_dir'] = __DIR__;
